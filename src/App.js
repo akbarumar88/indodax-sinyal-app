9d084e0cc30a5bc7809d64e07805981859e629cd
Akbar Umar Alfaro <akbarumar88@gmail.com>
2022-05-26 01:36:54 +0700

    - Berhasil menampilkan Data Indodax Gan.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,214 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { BASE_API } from "./GlobalVar"
+import { toCurrency } from "./helper/basic_helper"
 
 function App() {
+  let [data, setData] = useState([])
+  let [page, setPage] = useState(1)
+  let firstPage = page == 1
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_API}/all?page=${page}`)
+      .then(({ data }) => {
+        console.log("sukses", data)
+        setData(data)
+      })
+      .catch((err) => {
+        console.log("Err saat get data all", err.response?.data ?? err.message)
+      })
+  }, [page])
+
+  const nextPage = () => {
+    setPage(page + 1)
+  }
+
+  const prevPage = () => {setPage(page-1)}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.js
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <section className="relative pt-16 pb-0 bg-blueGray-50">
+        <div className="w-full mb-12 px-4">
+          <div
+            className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded 
+  bg-pink-900 text-white"
+          >
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1 ">
+                  <h3 className="font-semibold text-lg text-white">
+                    Data Sinyal Indodax
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="block w-full overflow-x-auto ">
+              <table className="items-center w-full bg-transparent border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      #{" "}
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      Sinyal
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Level
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Tanggal{" "}
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Harga (IDR)
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Harga (USDT)
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Volume (IDR)
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Volume (USDT)
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Last Buy
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Last Sell
+                    </th>
+                    <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-pink-800 text-pink-300 border-pink-700">
+                      {" "}
+                      Jenis
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {data.map(
+                    ({
+                      id,
+                      sinyal,
+                      level,
+                      tanggal,
+                      hargaidr,
+                      hargausdt,
+                      volidr,
+                      volusdt,
+                      lastbuy,
+                      lastsell,
+                      jenis,
+                    }) => {
+                      return (
+                        <tr>
+                          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {id}
+                          </th>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {sinyal}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {level}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {tanggal}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            Rp. {toCurrency(hargaidr)}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            $ {toCurrency(hargausdt)}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                            {volidr}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                            {volusdt}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                            {lastbuy}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                            {lastsell}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                            {jenis}
+                          </td>
+                        </tr>
+                      )
+                    }
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="flex flex-col items-center pb-12">
+        <span className="text-sm text-white dark:text-white">
+          Showing <span className="font-semibold text-white">1</span> to{" "}
+          <span className="font-semibold text-white">10</span> of{" "}
+          <span className="font-semibold text-white">100</span> Entries
+        </span>
+        {/* <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button> */}
+
+        <div className="inline-flex mt-2 xs:mt-0">
+          <button
+            onClick={prevPage}
+            disabled={firstPage}
+            className={`inline-flex items-center py-2 px-4 text-sm font-medium   rounded-l  focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-900 ${
+              firstPage
+                ? "cursor-not-allowed bg-slate-400 text-dark"
+                : "hover:bg-purple-800 bg-purple-700 text-white"
+            }`}
+          >
+            <svg
+              className="mr-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Prev
+          </button>
+          <button
+            onClick={nextPage}
+            className="inline-flex items-center py-2 px-4 text-sm font-medium text-white  rounded-r bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-900"
+          >
+            Next
+            <svg
+              className="ml-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/App_old.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg"
+import "./App.css"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { BASE_API } from "./GlobalVar"
+
+function App() {
+  let [data, setData] = useState([])
+  useEffect(() => {
+    axios
+      .get(`${BASE_API}/all`)
+      .then(({ data }) => {
+        console.log("sukses", data)
+      })
+      .catch((err) => {
+        console.log("Err saat get data all", err.response?.data ?? err.message)
+      })
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React.js
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
